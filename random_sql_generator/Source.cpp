45943c51fb43b1409ee015458e1fbc9cc56f6c80
mars-janda <65905992+mars-janda@users.noreply.github.com>
2020-11-07 22:59:25 +0100

    Upload of the Visual Studio Project File
    It works, might add some more data types in the future.

---
+++ random_sql_generator/Source.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <random>
+
+using namespace std;
+
+string random_string() {
+	string str("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+
+	random_device rd;
+	mt19937 generator(rd());
+
+	shuffle(str.begin(), str.end(), generator);
+
+	return str.substr(0, 12);     
+};
+
+int main() {
+	int len_arr_in;
+
+	cout << "Please input the number of attributes: ";
+	cin >> len_arr_in;
+
+	int len_arr = len_arr_in; 
+	int* data_types = new int[len_arr];
+	int i;
+
+	for (i = 0; i < len_arr; i++) {
+		cout << "Please input data type (0 = int, 1 = str) for attibute " << i + 1 << ": ";
+		cin >> data_types[i];
+	};
+
+	string table_name;
+	int instances_tbg;
+
+	cout << "Please input the number of instances to be generated: ";
+	cin >> instances_tbg;
+	cout << "Please input the name of the table: ";
+	cin >> table_name;
+
+	int itr = instances_tbg;
+	int g;
+
+	for (i = 0; i < itr; i++) {
+		cout << "INSERT INTO " << table_name << " VALUES (";
+		for (g = 0; g < len_arr; g++) {
+			if (data_types[g] == 1) {
+				cout << "'" << random_string() << "', ";
+			};
+			if (data_types[g] == 0) {
+				cout << rand() << ", ";
+			}
+		};
+		cout << ");" << endl;
+	};
+
+	return 0;
+}
